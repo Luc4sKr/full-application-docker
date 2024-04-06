7633e958702daf5c0b6fde67fb376a84e88eabbe
Luc4sKr <lucaskreuch77@gmail.com>
2024-04-06 19:33:37 -0300

    backend

---
+++ Backend/Backend.Api/Controllers/HealthCheck.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Api.Controllers
+{
+    [ApiController]
+[Route("api/[controller]")]
+    public class HealthCheck : Controller
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("message")]
+        public ActionResult<string> Message()
+        {
+            return Ok("Menssagem do Backend");
+        }
+    }
+}
